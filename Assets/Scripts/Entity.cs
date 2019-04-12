e4f7d6eda4092cf3f6077ed946254d780b65dda0
Robin <s1102585@student.windesheim.nl>
2019-04-12 12:00:26 +0200

    Healthbar
    Healthbar pic added,
    healthbar width bases on currenthealth

---
+++ Assets/Scripts/Entity.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Entity : MonoBehaviour
+{
+    [SerializeField] public int maxHealth;
+    [SerializeField] public int maxStamina;
+
+    private int health;
+    private int stamina;
+
+    private int normalize(int value, int max)
+    {
+        if (value >= max) return max;
+        else if (value <= 0) return 0;
+        else return value;
+    }
+
+    public int Health
+    {
+        get { return health; }
+        set { health = normalize(value, maxHealth); }
+    }
+
+    public int Stamina
+    {
+        get { return stamina; }
+        set { stamina = normalize(value, maxStamina); }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Health = maxHealth;
+        Stamina = maxStamina;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // TODO: Make stamina regain over time.
+    }
+
+    
+}
--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class HealthBar : MonoBehaviour
 {
+    public GameObject Entity;
+    private RectTransform Transform;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Transform = GetComponent<RectTransform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        SizeHealthBar(); // keep checking the size
+    }
+
+    public void SizeHealthBar() // function to check the healthbar and resize it if necesary
+    {
+        int currentHealth = Entity.GetComponent<Entity>().Health; // get currenthealth of entitiy
+        int maxHealth = Entity.GetComponent<Entity>().maxHealth; // get maxhealth of entity
+        float percentage = currentHealth / maxHealth * 100; // calculate percentage of health the entity has
+        float width = percentage / 100 * 200; // calculate width, 200 is normal width
+        Transform.sizeDelta = new Vector2(width,30); // height is set so it stays 30
     }
 }
