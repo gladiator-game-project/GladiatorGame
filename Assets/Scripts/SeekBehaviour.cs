422bd1b4d20021b8479a218211202ec3f84c2346
Robin <s1102585@student.windesheim.nl>
2019-04-16 11:32:20 +0200

    mooiere code en comments

--- Assets/Scripts/SeekBehaviour.cs
+++ Assets/Scripts/SeekBehaviour.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 public class SeekBehaviour : MonoBehaviour
 {
     public Vector3 Target;
-    public GameObject TargetObject; // u can either set an object to follow, or a position
+    public GameObject TargetObject; // You can either set an object to follow, or a position to follow
     public float Mass = 15; // mass of the entity, we could/should set this in the entity class later
-    public float MaxVelocity = 3; 
-    public float MaxForce = 15;
+    public float MaxVelocity = 3;  // just like this one
+    public float MaxForce = 15; // just like this one
 
     private Vector3 Velocity; // current velocity
 
@@ -23,14 +23,17 @@ public class SeekBehaviour : MonoBehaviour
     void Update()
     {
         if (TargetObject != null)
-            Target = TargetObject.transform.position; // if there is no object specified to go to, set a position to go to, which is standard itself
+            Target = TargetObject.transform.position; // if there is no object specified to go towards, set a position to go to, which is standard itself if not specified
 
         Vector3 DesiredVelocity = Target - transform.position; // calculate desired velocity
         DesiredVelocity = DesiredVelocity.normalized * MaxVelocity; // normalize it
+
         Vector3 Steering = DesiredVelocity - Velocity; // calculate steering
         Steering = Vector3.ClampMagnitude(Steering, MaxForce); // truncate
         Steering = Steering / Mass; // divide by mass
+
         Velocity = Vector3.ClampMagnitude(Velocity + Steering, MaxVelocity); // truncate velocity
+
         transform.position += Velocity * Time.deltaTime; // set new position
     }
 }
