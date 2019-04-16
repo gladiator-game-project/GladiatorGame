bebb0d965f24094321e18acd92796d9bd9319f40
Robin <s1102585@student.windesheim.nl>
2019-04-16 11:57:01 +0200

    code convesion i forgot

--- Assets/Scripts/SeekBehaviour.cs
+++ Assets/Scripts/SeekBehaviour.cs
@@ -6,16 +6,16 @@ public class SeekBehaviour : MonoBehaviour
 {
     public Vector3 Target;
     public GameObject TargetObject; // You can either set an object to follow, or a position to follow
-    public float Mass = 15; // mass of the entity, we could/should set this in the entity class later
-    public float MaxVelocity = 3;  // just like this one
-    public float MaxForce = 15; // just like this one
+    public float _mass = 15; // mass of the entity, we could/should set this in the entity class later
+    public float _maxVelocity = 3;  // just like this one
+    public float _maxForce = 15; // just like this one
 
-    private Vector3 Velocity; // current velocity
+    private Vector3 _velocity; // current velocity
 
     // Start is called before the first frame update
     void Start()
     {
-        Velocity = Vector3.zero; // velocity starts zero
+        _velocity = Vector3.zero; // velocity starts zero
         Target = transform.position; // set current position
     }
 
@@ -26,14 +26,14 @@ public class SeekBehaviour : MonoBehaviour
             Target = TargetObject.transform.position; // if there is no object specified to go towards, set a position to go to, which is standard itself if not specified
 
         Vector3 DesiredVelocity = Target - transform.position; // calculate desired velocity
-        DesiredVelocity = DesiredVelocity.normalized * MaxVelocity; // normalize it
+        DesiredVelocity = DesiredVelocity.normalized * _maxVelocity; // normalize it
 
-        Vector3 Steering = DesiredVelocity - Velocity; // calculate steering
-        Steering = Vector3.ClampMagnitude(Steering, MaxForce); // truncate
-        Steering = Steering / Mass; // divide by mass
+        Vector3 Steering = DesiredVelocity - _velocity; // calculate steering
+        Steering = Vector3.ClampMagnitude(Steering, _maxForce); // truncate
+        Steering = Steering / _mass; // divide by mass
 
-        Velocity = Vector3.ClampMagnitude(Velocity + Steering, MaxVelocity); // truncate velocity
+        _velocity = Vector3.ClampMagnitude(_velocity + Steering, _maxVelocity); // truncate velocity
 
-        transform.position += Velocity * Time.deltaTime; // set new position
+        transform.position += _velocity * Time.deltaTime; // set new position
     }
 }
