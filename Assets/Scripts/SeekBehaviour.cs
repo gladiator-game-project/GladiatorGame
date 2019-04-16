94da79e5e9b3bf0b08a84c3b1ef1a793a8a4fcbb
Robin <s1102585@student.windesheim.nl>
2019-04-16 11:57:57 +0200

    oeps foutje

--- Assets/Scripts/SeekBehaviour.cs
+++ Assets/Scripts/SeekBehaviour.cs
@@ -6,9 +6,9 @@ public class SeekBehaviour : MonoBehaviour
 {
     public Vector3 Target;
     public GameObject TargetObject; // You can either set an object to follow, or a position to follow
-    public float _mass = 15; // mass of the entity, we could/should set this in the entity class later
-    public float _maxVelocity = 3;  // just like this one
-    public float _maxForce = 15; // just like this one
+    public float Mass = 15; // mass of the entity, we could/should set this in the entity class later
+    public float MaxVelocity = 3;  // just like this one
+    public float MaxForce = 15; // just like this one
 
     private Vector3 _velocity; // current velocity
 
@@ -26,13 +26,13 @@ public class SeekBehaviour : MonoBehaviour
             Target = TargetObject.transform.position; // if there is no object specified to go towards, set a position to go to, which is standard itself if not specified
 
         Vector3 DesiredVelocity = Target - transform.position; // calculate desired velocity
-        DesiredVelocity = DesiredVelocity.normalized * _maxVelocity; // normalize it
+        DesiredVelocity = DesiredVelocity.normalized * MaxVelocity; // normalize it
 
         Vector3 Steering = DesiredVelocity - _velocity; // calculate steering
-        Steering = Vector3.ClampMagnitude(Steering, _maxForce); // truncate
-        Steering = Steering / _mass; // divide by mass
+        Steering = Vector3.ClampMagnitude(Steering, MaxForce); // truncate
+        Steering = Steering / Mass; // divide by mass
 
-        _velocity = Vector3.ClampMagnitude(_velocity + Steering, _maxVelocity); // truncate velocity
+        _velocity = Vector3.ClampMagnitude(_velocity + Steering, MaxVelocity); // truncate velocity
 
         transform.position += _velocity * Time.deltaTime; // set new position
     }
