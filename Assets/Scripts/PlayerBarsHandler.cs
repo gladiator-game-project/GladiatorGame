47155ed6d507829ac73a6599192060e08e5668e3
Bert Jan Wielink <bertjwielink@gmail.com>
2019-04-17 11:52:31 +0200

    Added UI prefab

---
+++ Assets/Scripts/PlayerBarsHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerBarsHandler : MonoBehaviour
+{
+    public Entity player;
+
+    private Image healthBlock;
+    private Image staminaBlock;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        healthBlock = transform.Find("Health Block").GetComponent<Image>();
+        staminaBlock = transform.Find("Stamina Block").GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        healthBlock.fillAmount = 1 - ((float)player.Health / (float)player.maxHealth);
+        staminaBlock.fillAmount = 1 - ((float)player.Stamina / (float)player.maxStamina);
+    }
+}
