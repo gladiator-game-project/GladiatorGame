f1d84c3bcf57774e85e4a701316087ad8295418c
foepje12 <jellevanommen@hotmail.nl>
2019-04-23 13:45:00 +0200

    Added attack behaviour
    Behaviour Bricks for behaviour trees.
    Initial setup for behaviour trees

---
+++ Assets/Scripts/Behaviour/Conditions/PlayerClose.cs
@@ -0,0 +1,18 @@
+﻿using BBUnity.Conditions;
+using Pada1.BBCore;
+using UnityEngine;
+
+[Condition("Perception/IsPlayerClose")]
+[Help("Checks whether the player is close depending on a given distance")]
+public class PlayerClose : GOCondition
+{
+    [InParam("player")]
+    [Help("Player to check the distance")]
+    public GameObject player;
+
+    public override bool Check()
+    {
+        var distance = Vector2.Distance(gameObject.transform.position, player.transform.position);
+        return distance < 1.5;
+    }
+}
