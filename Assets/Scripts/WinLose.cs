ac4731b288d14d4471dcb27c0da81dfa0e55ec95
Robin <s1102585@student.windesheim.nl>
2019-04-24 11:55:50 +0200

    added you win and you lose screen

--- Assets/Scripts/WinLose.cs
+++ Assets/Scripts/WinLose.cs
@@ -1,16 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class WinLose : MonoBehaviour
 {
     private GameObject Player;
     private Entity entityscript;
+    private PlayerMovement Movementscript;
+    public GameObject YouWinText;
+    private Text YouWinLose;
+    public GameObject RestartButton;
     // Start is called before the first frame update
     void Start()
     {
         Player = GameObject.Find("Player");
         entityscript = Player.GetComponent<Entity>();
+        YouWinLose = YouWinText.GetComponent<Text>();
+        Movementscript = Player.GetComponent<PlayerMovement>();
     }
 
     // Update is called once per frame
@@ -23,8 +30,11 @@ public class WinLose : MonoBehaviour
     {
         if (entityscript.Alive == false)
         {
-            Debug.Log("Game Over");
-            //TODO make game over screen?
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+            YouWinLose.text = "Game Over";
+            Movementscript.enabled = false;
+            RestartButton.SetActive(true);
         }
         GameObject[] Enemies = GameObject.FindGameObjectsWithTag("Enemy");
         bool allDeath = true; // assume all are death
@@ -39,8 +49,11 @@ public class WinLose : MonoBehaviour
 
         if (allDeath == true)
         {
-            Debug.Log("You win!");
-            //TODO make a you win screen\
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+            YouWinLose.text = "You win!";
+            Movementscript.enabled = false;
+            RestartButton.SetActive(true);
         }
     }
 }
