34561ab81b1a7204ee577db175faf601198e8eb1
donovan <donovanm37@gmail.com>
2019-05-08 12:53:49 +0200

    Main Menu

---
+++ Assets/Scripts/ExitGame.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class ExitGame : MonoBehaviour, IPointerClickHandler
+{
+    private float clickTime;
+    private int clickCount = 0;
+    public bool onClick = true;
+    public bool onDoubleClick = false;
+    private Text BtnText;
+    public void Start()
+    {
+        BtnText = transform.gameObject.GetComponentInChildren<Text>();
+    }
+
+    public void Update()
+    {
+
+    }
+    public void OnPointerClick(PointerEventData data)
+    {
+        // get interval between this click and the previous one (check for double click)
+        float interval = data.clickTime - clickTime;
+
+        // if this is double click, change click count
+        if (interval < 0.5 && interval > 0 && clickCount != 2)
+        {
+            clickCount = 2;
+        }
+        else
+        {
+            clickCount = 1;
+        }
+        // reset click time
+        clickTime = data.clickTime;
+
+        // single click
+        if (onClick && clickCount == 1)
+            Application.Quit();
+
+    }
+}
