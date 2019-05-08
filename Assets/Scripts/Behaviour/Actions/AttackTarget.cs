146d23fe3980729ce74693ac675e65e2208ce09b
foepje12 <jellevanommen@hotmail.nl>
2019-05-08 12:44:24 +0200

    Added movement behaviour
    - Moving direct to a target
    - Moving to the left of a target (circle)
    - Idle

---
+++ Assets/Scripts/Behaviour/Actions/AttackTarget.cs
@@ -0,0 +1,26 @@
+﻿using Pada1.BBCore.Tasks;
+using Pada1.BBCore;
+using UnityEngine;
+using BBUnity.Actions;
+
+[Action("Gladiator/AttackTarget")]
+[Help("Attacks the target")]
+public class AttackTarget : GOAction
+{
+    [InParam("Target")]
+    [Help("Target to check the distance")]
+    public GameObject Target;
+
+    public override TaskStatus OnUpdate()
+    {
+        Entity entity = Target.GetComponent<Entity>();
+
+        if (entity != null)
+        {
+            entity.LoseHealth(1);
+            return TaskStatus.COMPLETED;
+        }
+        return TaskStatus.FAILED;
+    }
+
+}
---
+++ Assets/Scripts/Behaviour/Conditions/FuzzyLogic.cs
@@ -0,0 +1,18 @@
+﻿using BBUnity.Conditions;
+using Pada1.BBCore;
+using UnityEngine;
+
+[Condition("FuzzyLogic")]
+[Help("Checks for a fuzzy logic condition")]
+public class FuzzyLogic : GOCondition
+{
+    [InParam("FuzzyConditions")]
+    [Help("Target to check the distance")]
+    public GameObject[] FuzzyConditions;
+
+    public override bool Check()
+    {
+        return true;
+    }
+}
+
