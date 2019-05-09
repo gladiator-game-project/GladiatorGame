85b778943112234c18fc0499fdc6a2e8b029f88d
SanderHagen <sanderhagen@outlook.com>
2019-05-09 15:09:31 +0200

    removed unnecessary boolean

--- Assets/Scripts/MenuButtons/CloseControlsButton.cs
+++ Assets/Scripts/MenuButtons/CloseControlsButton.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 
 public class CloseControlsButton : MonoBehaviour, IPointerClickHandler
 {   
-    public bool onClick = true;            
     private GameObject ControlsPanel;
 
     // Use this for initialization
@@ -18,14 +17,12 @@ public class CloseControlsButton : MonoBehaviour, IPointerClickHandler
     // Update is called once per frame
     public void OnPointerClick(PointerEventData data)
     {
-        // The Control panel is being removed from the screen
-        if (onClick)
-        {
+
             //The Control panel is being positioned at a 4x larger X-as position.
             Vector3 p = ControlsPanel.transform.position;
             Debug.Log(p.x);
             p.x = p.x * 4;
             ControlsPanel.transform.position = p;
-        }
+        
     }
 }
--- Assets/Scripts/MenuButtons/ShowControls.cs
+++ Assets/Scripts/MenuButtons/ShowControls.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 
 public class ShowControls : MonoBehaviour, IPointerClickHandler
 {
-    public bool onClick = true;      
     private GameObject ControlsPanel;
 
     // Use this for initialization
@@ -18,14 +17,11 @@ public class ShowControls : MonoBehaviour, IPointerClickHandler
     // Update is called once per frame
     public void OnPointerClick(PointerEventData data)
     {
-        // The Control panel is being placed on the screen
-        if (onClick)
-        {
             //The Control panel is being positioned at a 4x smaller X-as position
             Vector3 p = ControlsPanel.transform.position;
             Debug.Log(p.x);
             p.x = p.x / 4;
             ControlsPanel.transform.position = p;
-        }
+        
     }
 }
