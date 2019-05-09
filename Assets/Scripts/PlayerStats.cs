e84e93605c5662d834a3866408b3ff71c93c052c
Robin <s1102585@student.windesheim.nl>
2019-05-09 14:22:25 +0200

    coins and comment todo

--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -4,7 +4,14 @@ using UnityEngine;
 
 public class PlayerStats : MonoBehaviour
 {
+    public float Coins;
     // Start is called before the first frame update
+    private void Start()
+    {
+        //TODO: Playerprefs to save coins
+        Coins = 0.0f; // begin coins
+    }
+
     void Awake()
     {
         DontDestroyOnLoad(gameObject);
