c5bcbad73a1342b9200cdbe0cb69393150681022
SanderHagen <sanderhagen@outlook.com>
2019-05-09 15:10:41 +0200

    code style

--- Assets/Scripts/MenuButtons/CloseControlsButton.cs
+++ Assets/Scripts/MenuButtons/CloseControlsButton.cs
@@ -4,7 +4,7 @@ using UnityEngine.EventSystems;
 using UnityEngine;
 
 public class CloseControlsButton : MonoBehaviour, IPointerClickHandler
-{   
+{
     private GameObject ControlsPanel;
 
     // Use this for initialization
@@ -17,12 +17,10 @@ public class CloseControlsButton : MonoBehaviour, IPointerClickHandler
     // Update is called once per frame
     public void OnPointerClick(PointerEventData data)
     {
-
-            //The Control panel is being positioned at a 4x larger X-as position.
-            Vector3 p = ControlsPanel.transform.position;
-            Debug.Log(p.x);
-            p.x = p.x * 4;
-            ControlsPanel.transform.position = p;
-        
+        //The Control panel is being positioned at a 4x larger X-as position.
+        Vector3 p = ControlsPanel.transform.position;
+        Debug.Log(p.x);
+        p.x = p.x * 4;
+        ControlsPanel.transform.position = p;
     }
 }
--- Assets/Scripts/MenuButtons/ShowControls.cs
+++ Assets/Scripts/MenuButtons/ShowControls.cs
@@ -17,11 +17,10 @@ public class ShowControls : MonoBehaviour, IPointerClickHandler
     // Update is called once per frame
     public void OnPointerClick(PointerEventData data)
     {
-            //The Control panel is being positioned at a 4x smaller X-as position
-            Vector3 p = ControlsPanel.transform.position;
-            Debug.Log(p.x);
-            p.x = p.x / 4;
-            ControlsPanel.transform.position = p;
-        
+        //The Control panel is being positioned at a 4x smaller X-as position
+        Vector3 p = ControlsPanel.transform.position;
+        Debug.Log(p.x);
+        p.x = p.x / 4;
+        ControlsPanel.transform.position = p;
     }
 }
