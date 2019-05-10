08b2b02c98ee76a123c16efb0bd4c417ad2c7e8d
foepje12 <jellevanommen@hotmail.nl>
2019-05-10 12:22:11 +0200

    Added Courage
    - Moved some functions around

--- Assets/Scripts/Entity/Entity.cs
+++ Assets/Scripts/Entity/Entity.cs
@@ -11,7 +11,40 @@ public class Entity : MonoBehaviour
     [SerializeField] private GameObject Hand;
     private float health;
     private float stamina;
-    public bool Alive;
+    private float courage;
+    public bool Alive = true;
+
+    public float Health
+    {
+        get { return health; }
+        set { health = Mathf.Clamp(value, 0, maxHealth); }
+    }
+
+    public float Stamina
+    {
+        get { return stamina; }
+        set { stamina = Mathf.Clamp(value, 0, maxStamina); }
+    }
+
+    public float Courage
+    {
+        get => courage + health - 100;
+    }
+
+
+    public void Start()
+    {
+        Health = maxHealth;
+        Stamina = maxStamina;
+        courage = 100;
+        InvokeRepeating("RegainStamina", 2.0f, 2.0f); // repeat function
+    }
+
+    public void Update()
+    {
+        UpdateDeath();
+        UpdateWeapon();
+    }
 
     public void Attack(PlayerMovement.Direction direction)
     {
@@ -20,69 +53,19 @@ public class Entity : MonoBehaviour
 
     public bool LoseStamina(float stamina)
     {
-        if (Stamina - stamina > 0)
-        {
-            Stamina -= stamina;
-            return true;
-        } else
-        {
-            return false;
-        }
+        Stamina -= Stamina - stamina > 0 ? stamina : 0;
+        return Stamina - stamina > 0;
     }
-
-    /// <summary>
-    /// Keeps the value between the max and above 0.
-    /// </summary>
-    /// <param name="value">Value to normalize.</param>
-    /// <param name="max">Stop increasing at the max.</param>
-    /// <returns></returns>
-    private float Normalize(float value, float max)
-    {
-        if (value >= max) return max;
-        else if (value <= 0) return 0;
-        else return value;
-    }
-
-    public float Health
-    {
-        get { return health; }
-        set { health = Normalize(value, maxHealth); }
-    }
-
-    public float Stamina
-    {
-        get { return stamina; }
-        set { stamina = Normalize(value, maxStamina); }
-    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        Health = maxHealth;
-        Stamina = maxStamina;
-        Alive = true;
-        InvokeRepeating("RegainStamina", 2.0f, 2.0f); // repeat function
-    }
-
 
     private void RegainStamina()
     {
         Stamina += 20;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        UpdateDeath();
-        UpdateWeapon();
-    }
-
     private void UpdateDeath()
     {
         if (health <= 0)
-        {
             Alive = false;
-        }
     }
 
     private void UpdateWeapon()
