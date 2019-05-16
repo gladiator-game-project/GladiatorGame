a8d13e690f945bf75cd18400c2acdc86e6bd60e8
donovan <donovanm37@gmail.com>
2019-05-16 15:21:19 +0200

    Call the menu with P

--- Assets/Scripts/UI/MainMenuController.cs
+++ Assets/Scripts/UI/MainMenuController.cs
@@ -5,6 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour
 {
+    private GameObject InGamePanel;
+
     public void ExitGame() =>
         Application.Quit();
 
@@ -12,5 +14,19 @@ public class MainMenuController : MonoBehaviour
         SceneManager.LoadScene(1);
 
     public void ExitMatch() =>
-    SceneManager.LoadScene(0);
+        SceneManager.LoadScene(0);
+
+
+    private void Start()
+    {
+        InGamePanel = GameObject.Find("InGamePanel");
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            InGamePanel.SetActive(true);
+        }
+    }
 }
