93736327da7273291eeb17a0d66d9c80557a4673
SanderHagen <sanderhagen@outlook.com>
2019-05-23 15:51:07 +0200

    started on check if armor hit

--- Assets/Scripts/Entities/Player/DamageHandler.cs
+++ Assets/Scripts/Entities/Player/DamageHandler.cs
@@ -7,7 +7,8 @@ namespace Assets.Scripts.Entities.Player
 {
     public class DamageHandler : MonoBehaviour
     {
-        private List<Collider> _colliders; //The list with all colliders that are colliding with the weapon.
+        private List<Collider> _weaponColliders; //The list with all colliders that are colliding with the weapon.
+        private List<Collider> _armorColliders;
 
         private float _damageImmunity = 1f;
         private float _damageImmunityTimer;
@@ -15,7 +16,8 @@ namespace Assets.Scripts.Entities.Player
 
         void Start()
         {
-            _colliders = new List<Collider>(); //Create new List
+            _weaponColliders = new List<Collider>();
+            _armorColliders = new List<Collider>();
         }
 
         private void Update()
@@ -30,18 +32,19 @@ namespace Assets.Scripts.Entities.Player
         {
             var obj = col.GetComponentInParent<AnimationHandler>();
             var isWeapon = col.GetComponent<BaseWeapon>();
+            
 
             if (obj == null || isWeapon == null)
                 return;
 
-            if (gameObject != obj.gameObject && _colliders.Contains(col) == false)
-                _colliders.Add(col);
+            if (gameObject != obj.gameObject && _weaponColliders.Contains(col) == false)
+                _weaponColliders.Add(col);
         }
 
         public void OnTriggerExit(Collider col)
         {
-            if (_colliders.Contains(col))
-                _colliders.Remove(col);
+            if (_weaponColliders.Contains(col))
+                _weaponColliders.Remove(col);
         }
 
         private void UpdateTimer()
@@ -58,14 +61,16 @@ namespace Assets.Scripts.Entities.Player
         /// <summary>
         /// Checks if the entity itself is hit and by which weapon. 
         /// Then reduces hp by the amount of damage the attacking weapon does
+        /// This function checks which collider has hit us.
         /// </summary>
         private void CheckDamage()
         {
-            foreach (var c in _colliders) //For each collider check if it has an entity script and remove if it does
+            foreach (var c in _weaponColliders) //For each collider check if it has an entity script and remove if it does
             {
                 if (c.GetComponentInParent<AnimationHandler>().IsAnimationRunning("attack")) // Also check if animation is playing
                 {
-                    Debug.Log("Damage done");
+                   
+                    //TODO: check if armor is it: if armor hit, return
                     int damage = c.GetComponentInParent<WeaponHandler>().Weapon.damage; // retrieve damage done by the colliding weapon 
                     var healthScript = GetComponent<Entity>(); // Call entity script of the hit entity
                     healthScript.Health -= damage; // Call the LoseHealth function from entity script
@@ -73,7 +78,12 @@ namespace Assets.Scripts.Entities.Player
                     break;
                 }
             }
-            _colliders = new List<Collider>();
+            _weaponColliders = new List<Collider>();
+        }
+
+        private void CheckArmorHit()
+        {
+
         }
     }
 }
