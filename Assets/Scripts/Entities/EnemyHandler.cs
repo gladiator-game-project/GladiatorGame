8eb5a00ff966af225c30dafb1859f2e33b2a0503
Robin <s1102585@student.windesheim.nl>
2019-05-28 11:19:32 +0200

    ragdoll on death

---
+++ Assets/Scripts/Entities/EnemyHandler.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Assets.Scripts.Entities.Player;
+using Assets.Scripts.Entities;
+
+public class EnemyHandler : MonoBehaviour
+{
+    private Entity _entitiyscript;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _entitiyscript = GetComponent<Entity>();
+        SetRagdoll(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_entitiyscript.Alive == false)
+        {
+            SetRagdoll(true);
+            Destroy(this, 3f);
+        }
+    }
+
+    void SetRagdoll(bool onoff)
+    {
+        Rigidbody[] Rigidbodies = GetComponentsInChildren<Rigidbody>();
+        foreach (Rigidbody rigidbody in Rigidbodies)
+        {
+            rigidbody.isKinematic = !onoff;
+            rigidbody.useGravity = onoff;
+        }
+        GetComponent<BehaviorExecutor>().enabled = !onoff;
+        GetComponent<AnimationHandler>().enabled = !onoff;
+        GetComponent<Movement>().enabled = !onoff;
+        GetComponent<WeaponHandler>().enabled = !onoff;
+        GetComponent<DamageHandler>().enabled = !onoff;
+        GetComponent<Animator>().enabled = !onoff;
+    }
+}
