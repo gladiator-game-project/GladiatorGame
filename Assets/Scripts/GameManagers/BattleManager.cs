e1b2d37c81729710992aa5e892d2a2eac8673158
foepje12 <jellevanommen@hotmail.nl>
2019-06-04 11:42:35 +0200

    Moved player to lobby

---
+++ Assets/Scripts/GameManagers/BattleManager.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Assets.Scripts.Entities;
+using Assets.Scripts.Entities.Player;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.GameManagers
+{
+    public class BattleManager : MonoBehaviour
+    {
+        private GateManager _gateManager;
+        private Entity _player;
+
+        private List<Entity> _enemyList;        
+        private bool _battleGoing;
+
+
+        void Start()
+        {
+            _gateManager = GetComponent<GateManager>();
+            _player = GameObject.FindGameObjectWithTag("Player").GetComponent<Entity>();
+            _enemyList = new List<Entity>();
+        }
+
+        void Update()
+        {
+            if (_battleGoing)
+                CheckGameStatus();
+        }
+
+        /// <summary>
+        /// Start a new battle
+        /// </summary>
+        public void StartBattle()
+        {
+            //We can not start a new battle if there is one already
+            if (_battleGoing)
+                return;
+
+            SpawnEnemy(new Vector3(0, 0, 20));
+            _battleGoing = true;
+        }
+
+        /// <summary>
+        /// Check the status of the ongoing battle. 
+        /// </summary>
+        private void CheckGameStatus()
+        {
+            //Is the player Dead? end game
+            if (_player.Alive == false)
+            {
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+                _battleGoing = false;
+            }
+
+            //Remove every NPC who is dead
+            _enemyList.RemoveAll(enemy => enemy.GetComponent<Entity>().Alive == false);
+
+            //No more enemies? end game
+            if (_enemyList.Count == 0)
+            {
+                //Win screen
+                _battleGoing = false;
+                _gateManager.SetGates(true, false);
+                StartCoroutine(_player.EndOfBattle());
+            }
+        }
+
+        /// <summary>
+        /// Spawns an enemy at the given location
+        /// </summary>
+        /// <param name="SpawnLocation"></param>
+        private void SpawnEnemy(Vector3 SpawnLocation)
+        {
+            GameObject enemy = (GameObject)Instantiate(Resources.Load("Prefabs/NPC"));
+            enemy.transform.position = SpawnLocation;
+            enemy.GetComponent<Movement>().Target = _player.gameObject;
+
+            BehaviorExecutor behaviour = enemy.GetComponent<BehaviorExecutor>();
+            behaviour.SetBehaviorParam("Target", _player);
+
+            _enemyList.Add(enemy.GetComponent<Entity>());
+        }
+    }
+}
---
+++ Assets/Scripts/GameManagers/GateManager.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.GameManagers
+{
+    public class GateManager : MonoBehaviour
+    {
+        public GameObject Player;
+        public Animator Gate_Outer;
+        public Animator Gate_Inner;
+
+        private BattleManager _battleManager;
+        private bool _gateInnerOpen;
+        private bool _canToggle;
+
+        public void Start()
+        {
+            _battleManager = GetComponent<BattleManager>();
+            SetGates(false, true);
+        }
+
+        public void Update()
+        {
+            if (Player.transform.position.z > -56 && Player.transform.position.z < -44)
+            {
+                if (_canToggle)
+                {
+                    ToggleGates();
+                    _canToggle = false;
+                }
+            }
+            else if (_canToggle == false)
+                _canToggle = true;
+        }
+
+        public void ToggleGates()
+        {
+            if (_gateInnerOpen == false)            
+                _battleManager.StartBattle();            
+
+            SetGates(!_gateInnerOpen, _gateInnerOpen);
+        }
+
+        public void SetGates(bool innerOpen, bool outerOpen)
+        {
+            _gateInnerOpen = innerOpen;
+            Gate_Inner.SetBool("open", innerOpen);
+            Gate_Outer.SetBool("open", outerOpen);
+        }
+
+    }
+}
